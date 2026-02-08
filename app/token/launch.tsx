1654b2e60500be03925f86da8195b95c71037522
Tejaswa <tejaswahinduja@gmail.com>
2026-02-08 11:54:39 +0530

    ahh dekhnaa padega

--- app/token/launch.tsx
+++ app/token/launch.tsx
@@ -5,7 +5,35 @@ import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { MINT_SIZE, TOKEN_2022_PROGRAM_ID, createInitializeMint2Instruction, createMint, getMinimumBalanceForRentExemptMint } from "@solana/spl-token"
 import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
 
+async function handleTrans(){
+    const {connection}=useConnection();
+    const wallet=useWallet();
+    if(!wallet.publicKey){
+        return "please connect a wallet"
+    }
+    const keypair=Keypair.generate();
 
+    const lamports=await getMinimumBalanceForRentExemptMint(connection)
+    const transaction=new Transaction().add(
+        SystemProgram.createAccount({
+            fromPubkey:wallet.publicKey,
+            newAccountPubkey:keypair.publicKey,
+            space:MINT_SIZE,
+            lamports,
+            programId:TOKEN_2022_PROGRAM_ID
+
+        }
+    ),
+    createInitializeMint2Instruction(keypair.publicKey, 9, wallet.publicKey, wallet.publicKey, TOKEN_2022_PROGRAM_ID)
+    )
+    transaction.feePayer=wallet.publicKey
+    transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+    transaction.partialSign(keypair);
+
+    await wallet.sendTransaction(transaction, connection);
+    console.log(`Token mint created at ${keypair.publicKey.toBase58()}`)
+
+}
 type FormData={
     tokenName:string,
     tokenSymbol:string,
@@ -13,41 +41,11 @@ type FormData={
     initialSupply:Number
 }
 
-export async function Token(){
+export function Token(){
     const {register,handleSubmit}=useForm<FormData>();
     const onSubmit:SubmitHandler<FormData>=(data)=>{console.log(data)}
 
-
-
-    async function handleTrans(){
-        const {connection}=useConnection();
-        const wallet=useWallet();
-        if(!wallet.publicKey){
-            return "please connect a wallet"
-        }
-        const keypair=Keypair.generate();
-
-        const lamports=await getMinimumBalanceForRentExemptMint(connection)
-        const transaction=new Transaction().add(
-            SystemProgram.createAccount({
-                fromPubkey:wallet.publicKey,
-                newAccountPubkey:keypair.publicKey,
-                space:MINT_SIZE,
-                lamports,
-                programId:TOKEN_2022_PROGRAM_ID
-
-            }
-        ),
-        createInitializeMint2Instruction(keypair.publicKey, 9, wallet.publicKey, wallet.publicKey, TOKEN_2022_PROGRAM_ID)
-        )
-        transaction.feePayer=wallet.publicKey
-        transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
-        transaction.partialSign(keypair);
-
-        await wallet.sendTransaction(transaction, connection);
-        console.log(`Token mint created at ${keypair.publicKey.toBase58()}`)
-
-    }
+    
     return <div>
         <form onSubmit={handleSubmit(onSubmit)}>
         <Input placeholder="Enter Token name" {...register("tokenName")}/>
