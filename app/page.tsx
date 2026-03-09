f710e49f04a567eed939034de3fc4c3a17029d9c
Tejaswa <tejaswahinduja@gmail.com>
2026-03-09 13:40:39 +0530

    added twitter

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,26 @@
 "use client"
 
+import Link from "next/link";
 import { Token } from "./token/mintacc";
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 export default function Home() {
   return (
         <div className="min-h-screen bg-linear-to-br from-purple-50 to-blue-50">
-            {/* Header with Wallet Button */}
+             
+            
+               
+       
             <div className="container mx-auto px-4 py-6">
                 <div className="flex justify-between items-center">
+                    
                     <h1 className="text-2xl font-bold text-gray-800">Token Launchpad</h1>
+                    <Link  className="text-2xl font-bold"href="https://x.com/Tej_Codes">Twitter</Link>
                     <WalletMultiButton />
                 </div>
             </div>
             <Token />
+           
         </div>
     );
 }
